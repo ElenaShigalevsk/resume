71a8b4d70c676e8ca2834c900c0fb1506746da84
Elena Shyhalevsk <35922492+ElenaShigalevsk@users.noreply.github.com>
2024-10-02 04:53:24 +0300

    Add files via upload

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -52,6 +52,53 @@ import bankingBig from "./../img/projects/banking-big.png";
 import fitness from "./../img/projects/fitness.png";
 import fitnessBig from "./../img/projects/fitness-big.png";
 
+import goodques from "./../img/projects/goodques.png";
+import goodquesBig from "./../img/projects/goodques-big.png";
+
+import redFactory from "./../img/projects/redFactory.png";
+import redFactoryBig from "./../img/projects/redFactory-big.png";
+
+import cafedelites from "./../img/projects/cafedelites.png";
+import cafedelitesBig from "./../img/projects/cafedelites-big.png";
+
+import metacompliance from "./../img/projects/metacompliance.png";
+import metacomplianceBig from "./../img/projects/metacompliance-big.png";
+
+import infludata from "./../img/projects/infludata.png";
+import infludataBig from "./../img/projects/infludata-big.png";
+
+import flagship from "./../img/projects/flagship.png";
+import flagshipBig from "./../img/projects/flagship-big.png";
+
+import hairball from "./../img/projects/hairball.png";
+import hairballBig from "./../img/projects/hairball.png";
+
+import utvendigrenhold from "./../img/projects/utvendigrenhold.png";
+import utvendigrenholdBig from "./../img/projects/utvendigrenhold.png";
+
+import virginislandscharterboat from "./../img/projects/virginislandscharterboat.png";
+import virginislandscharterboatdBig from "./../img/projects/virginislandscharterboat-big.png";
+
+import rebeliron from "./../img/projects/rebeliron.png";
+import rebelironBig from "./../img/projects/rebeliron-big.png";
+
+import iso from "./../img/projects/iso.png";
+
+import click2order from "./../img/projects/click2order.png";
+
+import backlinks from "./../img/projects/backlinks.png";
+import backlinksBig from "./../img/projects/backlinks-big.png";
+
+import strambach from "./../img/projects/strambach.png";
+import strambachBig from "./../img/projects/strambach-big.png";
+
+import intel from "./../img/projects/intel.png";
+
+import more4apps from "./../img/projects/more4apps.png";
+import more4appsBig from "./../img/projects/more4apps-big.png";
+
+import integral from "./../img/projects/integral.png";
+
 const projects = [
 	{
 		title: 'Website for an international construction firm.',
@@ -214,6 +261,160 @@ const projects = [
 		imgBig: bankingBig,
 		gitHubLink: 'https://banking.dialen.com.ua',
 		link: 'banking'
+	},
+	{
+		title: 'Research Company ',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: goodques,
+		imgBig: goodquesBig,
+		gitHubLink: 'https://www.goodques.com',
+		link: 'goodques'
+	},
+	{
+		title: 'Red Factory Agency',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP, Elementor',
+		img: redFactory,
+		imgBig: redFactoryBig,
+		gitHubLink: 'https://red-factory.agency',
+		link: 'red-factory'
+	},
+	{
+		title: 'Easy Delicious Recipes',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP, WP Recipe Maker',
+		img: cafedelites,
+		imgBig: cafedelitesBig,
+		// gitHubLink: 'https://cafedelites.com',
+		gitHubLink: 'https://cafedelitescom.bigscoots-staging.com',
+		link: 'cafedelites'
+	},
+	{
+		title: 'Cyber Security Training & Software',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: metacompliance,
+		imgBig: metacomplianceBig,
+		gitHubLink: 'https://www.metacompliance.com',
+		link: 'metacompliance'
+	},
+	{
+		title: 'Search and Analyze Influencers & Creators',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP, Multilingual',
+		img: infludata,
+		imgBig: infludataBig,
+		gitHubLink: 'https://infludata.com',
+		link: 'infludata'
+	},
+	{
+		title: 'Creative Studio',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: flagship,
+		imgBig: flagshipBig,
+		gitHubLink: 'https://flagship.agency',
+		link: 'flagship'
+	},
+	{
+		title: 'Consulting Services',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: hairball,
+		imgBig: hairballBig,
+		gitHubLink: 'https://hairball.io',
+		link: 'hairball'
+	},
+	{
+		title: 'Professional cleaning services',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP, Elementor',
+		img: utvendigrenhold,
+		imgBig: utvendigrenholdBig,
+		gitHubLink: 'https://utvendigrenhold.no',
+		link: 'utvendigrenhold'
+	},
+	{
+		title: 'Virgin Islands Charter Boat',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: virginislandscharterboat,
+		imgBig: virginislandscharterboatdBig,
+		gitHubLink: 'https://virginislandscharterboat.com',
+		link: 'virginislandscharterboat'
+	},
+	{
+		title: 'GlobalStake',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: rebeliron,
+		imgBig: rebelironBig,
+		gitHubLink: 'https://rebeliron.com',
+		link: 'rebeliron'
+	},
+	{
+		title: 'Architectural Photography',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: iso,
+		imgBig: iso,
+		gitHubLink: 'https://iso-k.de',
+		link: 'iso'
+	},
+	{
+		title: 'QR Code Menu Creator',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: click2order,
+		imgBig: click2order,
+		gitHubLink: 'https://click2order.ch',
+		link: 'click2order'
+	},
+	{
+		title: 'Buy Back Links',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: backlinks,
+		imgBig: backlinksBig,
+		gitHubLink: 'https://back-links.ch',
+		link: 'backlinks'
+	},
+	{
+		title: 'Moving company Munich',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: strambach,
+		imgBig: strambachBig,
+		gitHubLink: 'https://strambach-websites.com',
+		link: 'strambach'
+	},
+	{
+		title: 'Consulting Services',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: intel,
+		imgBig: intel,
+		gitHubLink: 'https://intel-ledger.com',
+		link: 'intel'
+	},
+	{
+		title: 'More4apps is Your Excel Interface',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: more4apps,
+		imgBig: more4appsBig,
+		gitHubLink: 'https://more4apps.com',
+		link: 'more4apps'
+	},
+	{
+		title: 'Cybersecurity defender',
+		category: 'Wordpress',
+		skills: 'Wordpress, HTML, SCSS, JS, PHP',
+		img: integral,
+		imgBig: integral,
+		gitHubLink: 'https://integral-safe.com',
+		link: 'integral'
 	},
 
 ];
